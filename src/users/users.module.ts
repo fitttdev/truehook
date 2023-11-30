c26c5a71f35bae6d981dfe71fc28dfe822787a7e
unimafy <nima.yonten1729@gmail.com>
2023-11-30 18:46:07 +0600

    add users module

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { DbModule } from 'src/db';
+import { UserEntity, UserSchema } from './entities/user.entity';
 
 @Module({
+  imports: [
+    DbModule,
+    DbModule.forFeature([{ name: UserEntity.name, schema: UserSchema }]),
+  ],
   controllers: [UsersController],
   providers: [UsersService],
 })
